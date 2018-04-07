69dbdf6d11d6806e955381126867ba5960c4690c
raphagoo <33906573+raphagoo@users.noreply.github.com>
2018-04-07 18:27:29 +0200

    Accueil du site

---
+++ srcs/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf8">
+    <link rel="stylesheet" href="Assets/css/style.css">
+    <Title>Le Comptoir des Arts - Accueil</Title>
+</head>
+<?php require ('header.php') ?>
+<img id="tableauaccueil" src="Assets/IMG/tableau.jpg">
+<div id="commencer"><a href="index.php">Commencer la visite</a></div>

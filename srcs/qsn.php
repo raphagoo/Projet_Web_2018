f169003c1548f6a01d50c32dbf955e36ed1b141b
raphagoo <33906573+raphagoo@users.noreply.github.com>
2018-04-07 18:48:40 +0200

    Qui sommes-nous

---
+++ srcs/qsn.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+    <meta charset="utf8">
+    <link rel="stylesheet" href="Assets/css/style.css">
+    <Title>Le Comptoir des Arts - Qui sommes nous ?</Title>
+</head>
+<?php require ('header.php'); ?>
+<div id="qsn">
+<h1>Qui sommes nous ?</h1><br>
+<h4><p>
+    Le comptoir des Arts c'est le projet scolaire de 3 étudiants: Raphaël Agostini, Nathan Risbourg et Yohann Braun.<br>
+    Nous sommes tout les 3 actuellement en première année de Bachelor informatique à Aix-en-Provence.<br><br>
+    L'objectif pour ce projet est de créer un site d'e-commerce fonctionnel sur le thème de notre choix.<br><br>
+    Ici vous ne pourrez donc rien acheter.<br><br>
+    Vous pouvez cependant nous contacter si vous avez une remarque à nous faire concernant le siteou une question relative aux droits d'auteurs.
+</p></h4>
+</div>
+</body>
+</html>

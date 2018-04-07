71bf1fd0c61f619095e559a9b6fe508dd13a036b
raphagoo <33906573+raphagoo@users.noreply.github.com>
2018-04-07 18:25:02 +0200

    Header du site

---
+++ srcs/header.php
@@ -0,0 +1,16 @@
+
+<body>
+    <div class="container">
+        <img id="logoheader" width="100" height="100" src="Assets/css/icons/logonoir.png" alt="logonoir">
+        <div id="titre">
+        Le Comptoir des Arts
+        </div>
+        <nav>
+            <a class="menu" href="index.php">Gallerie</a>
+            <a class="menu" href="index.php">Qui Sommes-Nous ?</a>
+            <a class="menu" href="index.php">Contact</a>
+            <a class="menu" href="index.php">Connexion/Inscription</a>
+        </nav>
+    </div>
+</body>
+</html>

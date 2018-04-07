6c1bab1ad1df8b7f30197b76aa9f41230d91fdd7
raphagoo <33906573+raphagoo@users.noreply.github.com>
2018-04-07 18:48:20 +0200

    Accueil du site

--- srcs/index.php
+++ srcs/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<body>
 <head>
     <meta charset="utf8">
     <link rel="stylesheet" href="Assets/css/style.css">
@@ -8,3 +9,5 @@
 <?php require ('header.php') ?>
 <img id="tableauaccueil" src="Assets/IMG/tableau.jpg">
 <div id="commencer"><a href="index.php">Commencer la visite</a></div>
+</body>
+</html>

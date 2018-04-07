27850b11b9dc4819cee431ae8f722eff1717caa2
qtzzzz <qtzzz@hotmail.fr>
2018-04-07 18:18:44 +0200

    footer inc + maj docs

---
+++ iteration1/Srcs/include/footer.inc.php
@@ -0,0 +1,36 @@
+﻿<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>footer</title>
+    <link rel="stylesheet" href="../assets/css/footer.css">
+</head>
+<body>
+<div class="footer">
+    <div class="child" id="about">
+        <h1>A propos du comptoir</h1>
+        <h3>Qui sommes nous? </h3>
+        <h3>Mentions légales</h3>
+        <h3>Contactez nous</h3>
+    </div>
+    <hr class="vr">
+    <div class="child" id="oeuvres">
+        <h1>Oeuvres</h1>
+        <h3>Peinture</h3>
+        <h3>Dessin</h3>
+        <h3>Photographie</h3>
+        <h3>Edition</h3>
+    </div>
+    <hr class="vr">
+    <div class="child" id="infos">
+        <h1>Mon compte</h1>
+        <h3>Mes informations</h3>
+        <h3>Mes commandes</h3>
+    </div>
+</div>
+<div class="disclaimer">
+    <p>Le comptoir des Arts est un projet étudiant d'un site de vente d'Art. Nous ne detenons ni ne vendons aucun des
+        produits exposès sur le site. Les images utilisées apartiennent aux auteurs mentionnès</p>
+</div>
+</body>
+</html>
